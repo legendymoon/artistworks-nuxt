6574e3dce9150393710cb5b5ea96a085709e8ee5
legendy4141 <legendy4141@gmail.com>
2025-08-06 13:10:06 -0400

    chore: refactor vitest.config.ts for testing

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,13 +1,10 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import vue from '@vitejs/plugin-vue';
-import path from 'path';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import path from 'path';
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    tsconfigPaths(), // Add this line
-  ],
+  plugins: [vue(), tsconfigPaths()],
   test: {
     globals: true,
     environment: 'jsdom',
